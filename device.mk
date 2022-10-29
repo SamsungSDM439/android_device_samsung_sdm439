feb5b6084979cab124a5548233e9cde6bdd70cf8
Samsung Galaxy Spandragon 439 <115798295+SamsungSDM439@users.noreply.github.com>
2022-10-29 12:40:11 +0530

    add init and recovery configs

--- device.mk
+++ device.mk
@@ -80,3 +80,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.print.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.print.xml
+
+# Init
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.qcom.rc \
+    ueventd.qcom.rc
+
+# Recovery
+PRODUCT_PACKAGES += \
+    fastbootd \
+    init.recovery.qcom.rc
